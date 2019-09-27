0ec537afad6cf8d47b6d4081d9b09e8ea7da5dae
farazkaleemmalik <43715001+xxxFarazxxx@users.noreply.github.com>
2019-09-27 09:33:56 -0400

    Changed "clear" command format

--- clear/__init__.py
+++ clear/__init__.py
@@ -10,11 +10,17 @@ def execute(name, args):
 
     parser = argparse.ArgumentParser(description=helpString, prog=name)
     parser.add_argument("-f", "-force", help="Bypass confirmation", default=False, action="store_const", const=True, dest="f")
-    parser.add_argument("-t", "-title", help="Title of task. Deletes closest match if none are found", dest="title", default=False)
+    parser.add_argument("-a", "-all", help="Removes all todo list elements", default=False, action="store_const", const=True, dest="all")
+    parser.add_argument("title", help="Title of task. Deletes closest match if none are found")
 
     parsed = parser.parse_args(args)
-
-    if parsed.title and (parsed.f or input(colorama.Fore.RED + "Are you sure you want to delete " + parsed.title + " from the list? (N/y)").lower() == "y"):
+    if parsed.all and (parsed.f or input(colorama.Fore.RED + "Are you sure you want to delete the ENTIRE LIST? (N/y)").lower() == "y"):
+        with open("./.data/todo.json", "w") as todoFile:
+            todoFile.write(json.dumps({
+                "items": []
+            }))
+        print(colorama.Fore.RED + "Cleared!")
+    elif parsed.title and (parsed.f or input(colorama.Fore.RED + "Are you sure you want to delete " + parsed.title + " from the list? (N/y)").lower() == "y"):
         with open("./.data/todo.json", "r") as todoFile:
             todoDict = json.loads(todoFile.read())
 
@@ -28,20 +34,6 @@ def execute(name, args):
 
         with open("./.data/todo.json", "w") as todoFile:
             todoFile.write(json.dumps(todoDict))
-
-
-
-
-
-
-
-    elif parsed.f or input(colorama.Fore.RED + "Are you sure you want to delete the ENTIRE LIST? (N/y)").lower() == "y":
-        with open("./.data/todo.json", "w") as todoFile:
-            todoFile.write(json.dumps({
-                "items": []
-            }))
-        print(colorama.Fore.RED + "Cleared!")
-
 
 if __name__ == "__main__":
     pass
